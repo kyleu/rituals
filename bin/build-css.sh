cc5a2e23dae98352e9cf59a6b531377fd04cc600
Kyle Unverferth <kyle@kyleu.com>
2020-10-03 15:07:24 -0500

    Asset update

--- bin/build-css.sh
+++ bin/build-css.sh
@@ -7,5 +7,5 @@ set -e
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir/..
 
-sass --no-source-map web/stylesheets/style.scss web/assets/vendor/rituals.css
-sass --style=compressed --no-source-map web/stylesheets/style.scss web/assets/vendor/rituals.min.css
+sass --load-path=../npn/npnasset/stylesheets --no-source-map web/stylesheets/style.scss web/assets/vendor/rituals.css
+sass --load-path=../npn/npnasset/stylesheets --style=compressed --no-source-map web/stylesheets/style.scss web/assets/vendor/rituals.min.css
