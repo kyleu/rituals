37d6c0347ceab2fe35802249c89bc633cb783fc7
Kyle Unverferth <kyle@kyleu.com>
2023-04-03 13:22:27 -0600

    View bugfix

--- views/vestimate/vstory/vvote/Detail.html.go
+++ views/vestimate/vstory/vvote/Detail.html.go
@@ -87,7 +87,7 @@ func (p *Detail) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cutil.
 	qw422016.N().S(`
             <a title="Story" href="`)
 //line views/vestimate/vstory/vvote/Detail.html:33
-	qw422016.E().S(`/story` + `/` + p.Model.StoryID.String())
+	qw422016.E().S(`/estimate/story` + `/` + p.Model.StoryID.String())
 //line views/vestimate/vstory/vvote/Detail.html:33
 	qw422016.N().S(`">`)
 //line views/vestimate/vstory/vvote/Detail.html:33
