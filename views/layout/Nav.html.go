be76d6bd4b4908e0b0f4fea6f334afad89b52187
Kyle Unverferth <kyle@kyleu.com>
2023-10-07 10:52:18 -0600

    Nav icons, UI cleanup

--- views/layout/Nav.html.go
+++ views/layout/Nav.html.go
@@ -11,172 +11,170 @@ import (
 	"strings"
 
 	"github.com/kyleu/rituals/app"
-	"github.com/kyleu/rituals/app/controller/cmenu"
 	"github.com/kyleu/rituals/app/controller/cutil"
-	"github.com/kyleu/rituals/app/lib/menu"
 	"github.com/kyleu/rituals/views/components"
 	"github.com/kyleu/rituals/views/vutil"
 )
 
-//line views/layout/Nav.html:13
+//line views/layout/Nav.html:11
 import (
 	qtio422016 "io"
 
 	qt422016 "github.com/valyala/quicktemplate"
 )
 
-//line views/layout/Nav.html:13
+//line views/layout/Nav.html:11
 var (
 	_ = qtio422016.Copy
 	_ = qt422016.AcquireByteBuffer
 )
 
-//line views/layout/Nav.html:13
+//line views/layout/Nav.html:11
 func StreamNav(qw422016 *qt422016.Writer, as *app.State, ps *cutil.PageState) {
-//line views/layout/Nav.html:13
+//line views/layout/Nav.html:11
 	qw422016.N().S(`
 <nav id="navbar">
   <a class="logo" href="`)
-//line views/layout/Nav.html:15
+//line views/layout/Nav.html:13
 	qw422016.E().S(ps.RootPath)
-//line views/layout/Nav.html:15
+//line views/layout/Nav.html:13
 	qw422016.N().S(`" title="`)
-//line views/layout/Nav.html:15
+//line views/layout/Nav.html:13
 	qw422016.E().S(ps.RootTitle)
-//line views/layout/Nav.html:15
+//line views/layout/Nav.html:13
 	qw422016.N().S(` `)
-//line views/layout/Nav.html:15
+//line views/layout/Nav.html:13
 	qw422016.E().S(as.BuildInfo.String())
-//line views/layout/Nav.html:15
+//line views/layout/Nav.html:13
 	qw422016.N().S(`">`)
-//line views/layout/Nav.html:15
+//line views/layout/Nav.html:13
 	components.StreamSVGRef(qw422016, ps.RootIcon, 32, 32, ``, ps)
-//line views/layout/Nav.html:15
+//line views/layout/Nav.html:13
 	qw422016.N().S(`</a>
   <div class="breadcrumbs">
     <a class="link" href="`)
-//line views/layout/Nav.html:17
+//line views/layout/Nav.html:15
 	qw422016.E().S(ps.RootPath)
-//line views/layout/Nav.html:17
+//line views/layout/Nav.html:15
 	qw422016.N().S(`">`)
-//line views/layout/Nav.html:17
+//line views/layout/Nav.html:15
 	qw422016.E().S(ps.RootTitle)
-//line views/layout/Nav.html:17
+//line views/layout/Nav.html:15
 	qw422016.N().S(`</a>`)
-//line views/layout/Nav.html:17
-	StreamNavItems(qw422016, ps.Menu, ps.Breadcrumbs)
-//line views/layout/Nav.html:17
+//line views/layout/Nav.html:15
+	StreamNavItems(qw422016, ps)
+//line views/layout/Nav.html:15
 	qw422016.N().S(`
   </div>
 `)
-//line views/layout/Nav.html:19
+//line views/layout/Nav.html:17
 	if ps.SearchPath != "-" {
-//line views/layout/Nav.html:19
+//line views/layout/Nav.html:17
 		qw422016.N().S(`  <form action="`)
-//line views/layout/Nav.html:20
+//line views/layout/Nav.html:18
 		qw422016.E().S(ps.SearchPath)
-//line views/layout/Nav.html:20
+//line views/layout/Nav.html:18
 		qw422016.N().S(`" class="search" title="search">
     <input type="search" name="q" placeholder=" " />
     <div class="search-image" style="display: none;"><svg><use xlink:href="#svg-searchbox" /></svg></div>
   </form>
 `)
-//line views/layout/Nav.html:24
+//line views/layout/Nav.html:22
 	}
-//line views/layout/Nav.html:24
+//line views/layout/Nav.html:22
 	qw422016.N().S(`  <a class="profile" title="`)
-//line views/layout/Nav.html:25
+//line views/layout/Nav.html:23
 	qw422016.E().S(ps.AuthString())
-//line views/layout/Nav.html:25
+//line views/layout/Nav.html:23
 	qw422016.N().S(`" href="`)
-//line views/layout/Nav.html:25
+//line views/layout/Nav.html:23
 	qw422016.E().S(ps.ProfilePath)
-//line views/layout/Nav.html:25
+//line views/layout/Nav.html:23
 	qw422016.N().S(`">
 `)
-//line views/layout/Nav.html:26
+//line views/layout/Nav.html:24
 	if i := ps.Accounts.Image(); i != "" {
-//line views/layout/Nav.html:26
+//line views/layout/Nav.html:24
 		qw422016.N().S(`    <img style="width: 24px; height: 24px;" src="`)
-//line views/layout/Nav.html:27
+//line views/layout/Nav.html:25
 		qw422016.E().S(i)
-//line views/layout/Nav.html:27
+//line views/layout/Nav.html:25
 		qw422016.N().S(`" />
 `)
-//line views/layout/Nav.html:28
+//line views/layout/Nav.html:26
 	} else {
-//line views/layout/Nav.html:28
+//line views/layout/Nav.html:26
 		qw422016.N().S(`    `)
-//line views/layout/Nav.html:29
+//line views/layout/Nav.html:27
 		components.StreamSVGRef(qw422016, `profile`, 24, 24, ``, ps)
-//line views/layout/Nav.html:29
+//line views/layout/Nav.html:27
 		qw422016.N().S(`
 `)
-//line views/layout/Nav.html:30
+//line views/layout/Nav.html:28
 	}
-//line views/layout/Nav.html:30
+//line views/layout/Nav.html:28
 	qw422016.N().S(`  </a>
 `)
-//line views/layout/Nav.html:32
+//line views/layout/Nav.html:30
 	if !ps.HideMenu {
-//line views/layout/Nav.html:32
+//line views/layout/Nav.html:30
 		qw422016.N().S(`  <input type="checkbox" id="menu-toggle-input" style="display: none;" />
   <label class="menu-toggle" for="menu-toggle-input"><div class="spinner diagonal part-1"></div><div class="spinner horizontal"></div><div class="spinner diagonal part-2"></div></label>
   `)
-//line views/layout/Nav.html:35
+//line views/layout/Nav.html:33
 		StreamMenu(qw422016, ps)
-//line views/layout/Nav.html:35
+//line views/layout/Nav.html:33
 		qw422016.N().S(`
 `)
-//line views/layout/Nav.html:36
+//line views/layout/Nav.html:34
 	}
-//line views/layout/Nav.html:36
+//line views/layout/Nav.html:34
 	qw422016.N().S(`</nav>`)
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 }
 
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 func WriteNav(qq422016 qtio422016.Writer, as *app.State, ps *cutil.PageState) {
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 	qw422016 := qt422016.AcquireWriter(qq422016)
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 	StreamNav(qw422016, as, ps)
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 	qt422016.ReleaseWriter(qw422016)
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 }
 
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 func Nav(as *app.State, ps *cutil.PageState) string {
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 	qb422016 := qt422016.AcquireByteBuffer()
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 	WriteNav(qb422016, as, ps)
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 	qs422016 := string(qb422016.B)
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 	qt422016.ReleaseByteBuffer(qb422016)
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 	return qs422016
-//line views/layout/Nav.html:37
+//line views/layout/Nav.html:35
 }
 
-//line views/layout/Nav.html:39
-func StreamNavItems(qw422016 *qt422016.Writer, m menu.Items, breadcrumbs cmenu.Breadcrumbs) {
+//line views/layout/Nav.html:37
+func StreamNavItems(qw422016 *qt422016.Writer, ps *cutil.PageState) {
+//line views/layout/Nav.html:38
+	for idx, bc := range ps.Breadcrumbs {
 //line views/layout/Nav.html:40
-	for idx, bc := range breadcrumbs {
-//line views/layout/Nav.html:42
-		i := m.GetByPath(breadcrumbs[:idx+1])
+		i := ps.Menu.GetByPath(ps.Breadcrumbs[:idx+1])
 
-//line views/layout/Nav.html:44
+//line views/layout/Nav.html:42
 		vutil.StreamIndent(qw422016, true, 2)
-//line views/layout/Nav.html:44
+//line views/layout/Nav.html:42
 		qw422016.N().S(`<span class="separator">/</span>`)
-//line views/layout/Nav.html:46
+//line views/layout/Nav.html:44
 		vutil.StreamIndent(qw422016, true, 2)
-//line views/layout/Nav.html:47
+//line views/layout/Nav.html:45
 		if i == nil {
-//line views/layout/Nav.html:49
+//line views/layout/Nav.html:47
 			bcLink := ""
 			if strings.Contains(bc, "||") {
 				bci := strings.Index(bc, "||")
@@ -184,57 +182,61 @@ func StreamNavItems(qw422016 *qt422016.Writer, m menu.Items, breadcrumbs cmenu.B
 				bc = bc[:bci]
 			}
 
+//line views/layout/Nav.html:53
+			qw422016.N().S(`<a class="link" href="`)
+//line views/layout/Nav.html:54
+			qw422016.E().S(bcLink)
+//line views/layout/Nav.html:54
+			qw422016.N().S(`">`)
+//line views/layout/Nav.html:54
+			components.StreamSVGRef(qw422016, "play", 28, 28, "breadcrumb-icon", ps)
+//line views/layout/Nav.html:54
+			qw422016.E().S(bc)
+//line views/layout/Nav.html:54
+			qw422016.N().S(`</a>`)
+//line views/layout/Nav.html:55
+		} else {
 //line views/layout/Nav.html:55
 			qw422016.N().S(`<a class="link" href="`)
 //line views/layout/Nav.html:56
-			qw422016.E().S(bcLink)
-//line views/layout/Nav.html:56
-			qw422016.N().S(`">`)
-//line views/layout/Nav.html:56
-			qw422016.E().S(bc)
-//line views/layout/Nav.html:56
-			qw422016.N().S(`</a>`)
-//line views/layout/Nav.html:57
-		} else {
-//line views/layout/Nav.html:57
-			qw422016.N().S(`<a class="link" href="`)
-//line views/layout/Nav.html:58
 			qw422016.E().S(i.Route)
-//line views/layout/Nav.html:58
+//line views/layout/Nav.html:56
 			qw422016.N().S(`">`)
-//line views/layout/Nav.html:58
+//line views/layout/Nav.html:56
+			components.StreamSVGRef(qw422016, i.Icon, 28, 28, "breadcrumb-icon", ps)
+//line views/layout/Nav.html:56
 			qw422016.E().S(i.Title)
-//line views/layout/Nav.html:58
+//line views/layout/Nav.html:56
 			qw422016.N().S(`</a>`)
-//line views/layout/Nav.html:59
+//line views/layout/Nav.html:57
 		}
-//line views/layout/Nav.html:60
+//line views/layout/Nav.html:58
 	}
-//line views/layout/Nav.html:61
+//line views/layout/Nav.html:59
 }
 
-//line views/layout/Nav.html:61
-func WriteNavItems(qq422016 qtio422016.Writer, m menu.Items, breadcrumbs cmenu.Breadcrumbs) {
-//line views/layout/Nav.html:61
+//line views/layout/Nav.html:59
+func WriteNavItems(qq422016 qtio422016.Writer, ps *cutil.PageState) {
+//line views/layout/Nav.html:59
 	qw422016 := qt422016.AcquireWriter(qq422016)
-//line views/layout/Nav.html:61
-	StreamNavItems(qw422016, m, breadcrumbs)
-//line views/layout/Nav.html:61
+//line views/layout/Nav.html:59
+	StreamNavItems(qw422016, ps)
+//line views/layout/Nav.html:59
 	qt422016.ReleaseWriter(qw422016)
-//line views/layout/Nav.html:61
+//line views/layout/Nav.html:59
 }
 
-//line views/layout/Nav.html:61
-func NavItems(m menu.Items, breadcrumbs cmenu.Breadcrumbs) string {
-//line views/layout/Nav.html:61
+//line views/layout/Nav.html:59
+func NavItems(ps *cutil.PageState) string {
+//line views/layout/Nav.html:59
 	qb422016 := qt422016.AcquireByteBuffer()
-//line views/layout/Nav.html:61
-	WriteNavItems(qb422016, m, breadcrumbs)
-//line views/layout/Nav.html:61
+//line views/layout/Nav.html:59
+	WriteNavItems(qb422016, ps)
+//line views/layout/Nav.html:59
 	qs422016 := string(qb422016.B)
-//line views/layout/Nav.html:61
+//line views/layout/Nav.html:59
 	qt422016.ReleaseByteBuffer(qb422016)
-//line views/layout/Nav.html:61
+//line views/layout/Nav.html:59
 	return qs422016
-//line views/layout/Nav.html:61
+//line views/layout/Nav.html:59
 }
--- views/vadmin/Settings.html.go
+++ views/vadmin/Settings.html.go
@@ -97,13 +97,14 @@ func (p *Settings) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cuti
 //line views/vadmin/Settings.html:28
 	}
 //line views/vadmin/Settings.html:28
-	qw422016.N().S(`  </div>
+	qw422016.N().S(`    <em>This page is for the settings of the application. To change your user preferences, such as themes, <a href="/profile">edit your profile</a>.</em>
+  </div>
 
   <div class="card">
     <h3>`)
-//line views/vadmin/Settings.html:32
+//line views/vadmin/Settings.html:33
 	components.StreamSVGRefIcon(qw422016, `database`, ps)
-//line views/vadmin/Settings.html:32
+//line views/vadmin/Settings.html:33
 	qw422016.N().S(`Database Management</h3>
     <ul class="mt">
       <li><a href="/admin/database">Database management</a></li>
@@ -112,186 +113,186 @@ func (p *Settings) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cuti
 
   <div class="card">
     <h3>`)
-//line views/vadmin/Settings.html:39
+//line views/vadmin/Settings.html:40
 	components.StreamSVGRefIcon(qw422016, `archive`, ps)
-//line views/vadmin/Settings.html:39
+//line views/vadmin/Settings.html:40
 	qw422016.N().S(`Admin Functions</h3>
     `)
-//line views/vadmin/Settings.html:40
+//line views/vadmin/Settings.html:41
 	streamsettingsLink(qw422016, "/admin/server", "archive", "App Information", "All sorts of info about the server and runtime", ps)
-//line views/vadmin/Settings.html:40
+//line views/vadmin/Settings.html:41
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:41
+//line views/vadmin/Settings.html:42
 	streamsettingsLink(qw422016, "/admin/modules", "archive", "Go Modules", "The Go modules used by "+util.AppName, ps)
-//line views/vadmin/Settings.html:41
+//line views/vadmin/Settings.html:42
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:42
+//line views/vadmin/Settings.html:43
 	streamsettingsLink(qw422016, "/admin/migrations", "archive", "Database Migrations", "Shows the full content of all database SQL migrations", ps)
-//line views/vadmin/Settings.html:42
+//line views/vadmin/Settings.html:43
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:43
+//line views/vadmin/Settings.html:44
 	streamsettingsLink(qw422016, "/theme", "archive", "Edit Themes", "Configure the design themes available to end users", ps)
-//line views/vadmin/Settings.html:43
+//line views/vadmin/Settings.html:44
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:44
+//line views/vadmin/Settings.html:45
 	streamsettingsLink(qw422016, "/admin/logs", "archive", "Recent Logs", "Displays the 100 most recent app logs", ps)
-//line views/vadmin/Settings.html:44
+//line views/vadmin/Settings.html:45
 	qw422016.N().S(`
     <div class="clear"></div>
   </div>
   <div class="card">
     <h3>`)
-//line views/vadmin/Settings.html:48
+//line views/vadmin/Settings.html:49
 	components.StreamSVGRefIcon(qw422016, `bolt`, ps)
-//line views/vadmin/Settings.html:48
+//line views/vadmin/Settings.html:49
 	qw422016.N().S(`HTTP Methods</h3>
     `)
-//line views/vadmin/Settings.html:49
+//line views/vadmin/Settings.html:50
 	streamsettingsLink(qw422016, "/admin/sitemap", "bolt", "Sitemap", "Displays the HTTP actions that are available, with documentation", ps)
-//line views/vadmin/Settings.html:49
+//line views/vadmin/Settings.html:50
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:50
+//line views/vadmin/Settings.html:51
 	streamsettingsLink(qw422016, "/admin/routes", "bolt", "HTTP routes", "Enumerates all registered HTTP routes, by method", ps)
-//line views/vadmin/Settings.html:50
+//line views/vadmin/Settings.html:51
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:51
+//line views/vadmin/Settings.html:52
 	streamsettingsLink(qw422016, "/admin/session", "bolt", "User Session", "View the user session, including all cookies and settings", ps)
-//line views/vadmin/Settings.html:51
+//line views/vadmin/Settings.html:52
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:52
+//line views/vadmin/Settings.html:53
 	streamsettingsLink(qw422016, "/admin/request", "bolt", "Debug HTTP Request", "Full debug view of an HTTP request from your browser", ps)
-//line views/vadmin/Settings.html:52
+//line views/vadmin/Settings.html:53
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:53
+//line views/vadmin/Settings.html:54
 	streamsettingsLink(qw422016, "/admin/sockets", "bolt", "Active WebSockets", "Manage the active WebSockets in this server", ps)
-//line views/vadmin/Settings.html:53
+//line views/vadmin/Settings.html:54
 	qw422016.N().S(`
     <div class="clear"></div>
   </div>
   <div class="card">
     <h3>`)
-//line views/vadmin/Settings.html:57
+//line views/vadmin/Settings.html:58
 	components.StreamSVGRefIcon(qw422016, `cog`, ps)
-//line views/vadmin/Settings.html:57
+//line views/vadmin/Settings.html:58
 	qw422016.N().S(`App Profiling</h3>
     `)
-//line views/vadmin/Settings.html:58
+//line views/vadmin/Settings.html:59
 	streamsettingsLink(qw422016, "/admin/memusage", "cog", "Memory Usage", "Detailed memory usage statistics", ps)
-//line views/vadmin/Settings.html:58
+//line views/vadmin/Settings.html:59
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:59
+//line views/vadmin/Settings.html:60
 	streamsettingsLink(qw422016, "/admin/gc", "cog", "Collect Garbage", "Runs the Go garbage collector", ps)
-//line views/vadmin/Settings.html:59
+//line views/vadmin/Settings.html:60
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:60
+//line views/vadmin/Settings.html:61
 	streamsettingsLink(qw422016, "/admin/heap", "cog", "Write Memory Dump", "Writes a memory dump to <em>./tmp/mem.pprof</em>, use script to view", ps)
-//line views/vadmin/Settings.html:60
+//line views/vadmin/Settings.html:61
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:61
+//line views/vadmin/Settings.html:62
 	streamsettingsLink(qw422016, "/admin/cpu/start", "cog", "Start CPU Profile", "Profiles the CPU using <em>./tmp/cpu.pprof</em>, use script to view", ps)
-//line views/vadmin/Settings.html:61
+//line views/vadmin/Settings.html:62
 	qw422016.N().S(`
     `)
-//line views/vadmin/Settings.html:62
+//line views/vadmin/Settings.html:63
 	streamsettingsLink(qw422016, "/admin/cpu/stop", "cog", "Stop CPU Profile", "Stops the active CPU profile", ps)
-//line views/vadmin/Settings.html:62
+//line views/vadmin/Settings.html:63
 	qw422016.N().S(`
     <div class="clear"></div>
   </div>
 
   `)
-//line views/vadmin/Settings.html:66
+//line views/vadmin/Settings.html:67
 	vauth.StreamAuthentication(qw422016, as, ps)
-//line views/vadmin/Settings.html:66
+//line views/vadmin/Settings.html:67
 	qw422016.N().S(`
 
   `)
-//line views/vadmin/Settings.html:68
+//line views/vadmin/Settings.html:69
 	vauth.StreamPermissions(qw422016, p.Perms, as)
-//line views/vadmin/Settings.html:68
+//line views/vadmin/Settings.html:69
 	qw422016.N().S(`
 `)
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 }
 
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 func (p *Settings) WriteBody(qq422016 qtio422016.Writer, as *app.State, ps *cutil.PageState) {
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 	qw422016 := qt422016.AcquireWriter(qq422016)
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 	p.StreamBody(qw422016, as, ps)
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 	qt422016.ReleaseWriter(qw422016)
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 }
 
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 func (p *Settings) Body(as *app.State, ps *cutil.PageState) string {
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 	qb422016 := qt422016.AcquireByteBuffer()
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 	p.WriteBody(qb422016, as, ps)
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 	qs422016 := string(qb422016.B)
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 	qt422016.ReleaseByteBuffer(qb422016)
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 	return qs422016
-//line views/vadmin/Settings.html:69
+//line views/vadmin/Settings.html:70
 }
 
-//line views/vadmin/Settings.html:71
+//line views/vadmin/Settings.html:72
 func streamsettingsLink(qw422016 *qt422016.Writer, href string, icon string, title string, description string, ps *cutil.PageState) {
-//line views/vadmin/Settings.html:71
+//line views/vadmin/Settings.html:72
 	qw422016.N().S(`<hr class="clear" /><div class="mts"><a href="`)
-//line views/vadmin/Settings.html:74
+//line views/vadmin/Settings.html:75
 	qw422016.E().S(href)
-//line views/vadmin/Settings.html:74
+//line views/vadmin/Settings.html:75
 	qw422016.N().S(`"><strong>`)
-//line views/vadmin/Settings.html:74
+//line views/vadmin/Settings.html:75
 	qw422016.E().S(title)
-//line views/vadmin/Settings.html:74
+//line views/vadmin/Settings.html:75
 	qw422016.N().S(`</strong></a><div><em>`)
-//line views/vadmin/Settings.html:75
+//line views/vadmin/Settings.html:76
 	qw422016.N().S(description)
-//line views/vadmin/Settings.html:75
+//line views/vadmin/Settings.html:76
 	qw422016.N().S(`</em></div></div>`)
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 }
 
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 func writesettingsLink(qq422016 qtio422016.Writer, href string, icon string, title string, description string, ps *cutil.PageState) {
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 	qw422016 := qt422016.AcquireWriter(qq422016)
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 	streamsettingsLink(qw422016, href, icon, title, description, ps)
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 	qt422016.ReleaseWriter(qw422016)
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 }
 
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 func settingsLink(href string, icon string, title string, description string, ps *cutil.PageState) string {
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 	qb422016 := qt422016.AcquireByteBuffer()
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 	writesettingsLink(qb422016, href, icon, title, description, ps)
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 	qs422016 := string(qb422016.B)
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 	qt422016.ReleaseByteBuffer(qb422016)
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 	return qs422016
-//line views/vadmin/Settings.html:77
+//line views/vadmin/Settings.html:78
 }
