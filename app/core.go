6f2d577cd1cc56c6713e7d668647f068c61210fc
Kyle Unverferth <kyle@kyleu.com>
2024-06-19 13:57:55 -0600

    Tests, linting

--- app/core.go
+++ app/core.go
@@ -11,7 +11,6 @@ type CoreServices struct {
 	Socket *websocket.Service
 }
 
-//nolint:revive
 func initCoreServices(ctx context.Context, st *State, logger util.Logger) CoreServices {
 	return CoreServices{
 		Socket: websocket.NewService(nil, nil),
